5d4c25fa31f21acf4193ae08806a340ea6605137
Sanjusheoran375 <sanjusheoran375@gmail.com>
2024-11-30 07:12:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,8 +9,8 @@ SESSION_ID: process.env.SESSION_ID || "L7pFRIrC#9w1mAhodbeFnyRwd5xJk92pyjijdHduz
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
-AUTO_STICKER: process.env.AUTO_STICKER || "false",
-AUTO_REPLY: process.env.AUTO_REPLY || "false",
+AUTO_STICKER: process.env.AUTO_STICKER || "true",
+AUTO_REPLY: process.env.AUTO_REPLY || "true",
 AUTO_REACT: process.env.AUTO_REACT || "true",
 FAKE_RECORDING: process.env.FAKE_RECORDING || "true",    
 };
